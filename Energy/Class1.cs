d45a241ea02f934215a2d7ba2029edacc81bbcd5
ayoyo <ayoyo@DESKTOP-ILKBPQ6>
2022-04-16 14:33:03 +0300

    Добавьте файлы проекта.

---
+++ Energy/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Energy
+{
+    public class Formula
+    {
+        public double e_count(double m, double v)
+        {
+            double e = (m * Math.Pow(v, 2)) / 2;
+
+            return e;
+        }
+
+        internal static string e_count(string text1, string text2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Energy/Energy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Energy
+{
+    public partial class Energy : Form
+    {
+        public Energy()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Formula en = new Formula();
+
+            double v2;
+            Double.TryParse(textBox1.Text,out v2);
+            double m2;
+            Double.TryParse(textBox2.Text,out m2);
+
+            textBox3.Text = en.e_count(v2, m2).ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Energy_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
